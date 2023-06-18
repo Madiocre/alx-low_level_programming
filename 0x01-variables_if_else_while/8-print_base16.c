805614eba377b34ae477c5e331b66e53e2130f4c
Madiocre <Madiocre>
2023-06-18 12:14:16 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Prints base 16 (hexa)
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    int i = 0;
+
+    while (i < 10)
+    {
+        putchar(i + '0');
+        i++;
+    }
+
+    i = 0;
+    while (i < 6)
+    {
+        putchar(i + 'a');
+        i++;
+    }
+
+    putchar('\n');
+    return (0);
+}
