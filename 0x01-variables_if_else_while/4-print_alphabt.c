1bb0c36bc44b991bfacee6b6d155cd434e255fc6
Madiocre <Madiocre>
2023-06-18 11:02:32 -0700

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet lowercase
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+if (letter != 'e' && letter != 'q')
+{
+putchar(letter);
+}
+letter++;
+}
+putchar('\n');
+return (0);
+}
