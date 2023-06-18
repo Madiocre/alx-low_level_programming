640b25650ee00e0c95888bb1a91dcd8da0e3a968
Madiocre <Madiocre>
2023-06-18 05:15:37 -0700

    puts c_code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Description: 'printing a line using command puts'
+ * @parameter: no idea
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
