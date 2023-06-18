d994ea68ba8f2a820aca592a47cd1eab7a2bcf2a
Madiocre <Madiocre>
2023-06-18 05:13:16 -0700

    puts c_code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-//This code prints the sentense '"Programming is like building a multilingual puzzle' exactly using puts
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
