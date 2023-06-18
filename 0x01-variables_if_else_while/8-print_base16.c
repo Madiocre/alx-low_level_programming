8f904a9e1cdc1d1f0267b6f385a3bfc1598582da
Madiocre <Madiocre>
2023-06-18 12:15:30 -0700

    hexa

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,21 +6,18 @@
  */
 int main(void)
 {
-    int i = 0;
-
-    while (i < 10)
-    {
-        putchar(i + '0');
-        i++;
-    }
-
-    i = 0;
-    while (i < 6)
-    {
-        putchar(i + 'a');
-        i++;
-    }
-
-    putchar('\n');
-    return (0);
+int i = 0;
+while (i < 10)
+{
+putchar(i + '0');
+i++;
+}
+i = 0;
+while (i < 6)
+{
+putchar(i + 'a');
+i++;
+}
+putchar('\n');
+return (0);
 }
