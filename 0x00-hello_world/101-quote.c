b891c1e0f2c01d0fdd348c91444207efc2a3e543
Madiocre <Madiocre>
2023-06-18 07:52:53 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+const char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, quote, sizeof(quote) - 1);
+return (1);
+}
