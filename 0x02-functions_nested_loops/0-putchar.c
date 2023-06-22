06468d55a6ee087a14e494b6375896ff4015abfa
Madiocre <Madiocre>
2023-06-21 18:17:51 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,13 @@
 #include <string.h>
 #include "main.h"
 /**
- *
  * main - Prints '_putchar' in string
  *
  * Return: 0 on success
  */
 int main(void)
 {
-print_putchar();
+char s[] = "_putchar";
+print_putchar(s);
 return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #include <unistd.h>
-
+#include <string.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -8,19 +8,16 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
-void print_putchar(void)
+void print_putchar(char s[])
 {
 /**
 Use 6 calls to _putchar to print '_putchar' 
 */
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+int i = 0;
+for(i = 0; i < strlen(s); i++)
+{
+_putchar(s[i]);
+}
 _putchar('\n');
 /**
  * Print a newline after '_putchar'
