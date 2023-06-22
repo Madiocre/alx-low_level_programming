668e6d0a0c65405c6ea1d7bee336d857167cfa70
Madiocre <Madiocre>
2023-06-21 18:46:55 -0700

    alphabcde

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,17 +8,5 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
-void print_putchar(char s[])
-{
-int i = 0;
-int len = strlen(s);
-for(i = 0; i < len; i++)
-{
-_putchar(s[i]);
-}
-_putchar('\n');
-/**
- * Print a newline after '_putchar'
- */
-}
+void print_putchar(char s[]);
 void print_alphabet(void);
