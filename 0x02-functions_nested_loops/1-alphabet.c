74482bc0c08570464787b1b0e63cf4182a11bbdb
Madiocre <Madiocre>
2023-06-21 18:53:56 -0700

    alphabcde

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * main - check the code
- * description - Prints alphabet then new line
- *
- * Return: Always 0.
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * 
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 char s[] = "abcdefghijklmnopqrstuvwxyz";
 int i = 0;
 int len = strlen(s);
-for(i = 0; i < len; i++)
+for (i = 0; i < len; i++)
 {
 _putchar(s[i]);
 }
