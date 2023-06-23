e9cd774f768c4cfbcedbdcdbeb2ff8b1741ce767
Madiocre <Madiocre>
2023-06-22 17:42:35 -0700

    debug1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,8 +5,12 @@
 */
 int main(void)
 {
+/* int initialization */
 int i;
+/* we make the value of i to 0*/
 i = 0;
+/* we test it on the function*/
 positive_or_negative(i);
+/* return zero*/
 return (0);
 }
