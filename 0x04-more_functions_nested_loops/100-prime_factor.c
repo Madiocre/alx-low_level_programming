7c18d5dc5baa18568fd9ac104371292bf98e2338
Madiocre <Madiocre>
2023-06-23 17:57:01 -0700

    prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - print largest prime number
+ * Return: (0) on success
+ */
+int main(void)
+{
+long int n = 612852475143L;
+long int largest_prime = 2;
+
+while (n % 2 == 0)
+{
+n /= 2;
+}
+
+for (long int i = 3; i <= sqrt(n); i += 2)
+{
+while (n % i == 0)
+{
+largest_prime = i;
+n /= i;
+}
+}
+
+if (n > 2) 
+{
+largest_prime = n;
+}
+
+printf("%ld\n", largest_prime);
+return (0);
+}
