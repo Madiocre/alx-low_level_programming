04e1b5324a95c068cb6f0ae34c9b382fd746ff85
Madiocre <Madiocre>
2023-06-26 15:43:42 -0700

    98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,13 @@
 #include "main.h"
+
 /**
- * main - Changes number to 98
- * no Return
+ * reset_to_98 - Changes the value of an integer to 98.
+ *
+ * @n: A pointer to the integer to be changed
+ *
+ * Return: No return value (void).
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
