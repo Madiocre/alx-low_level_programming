56d64e0873f45e79fd3740afc304e52603b00834
Madiocre <Madiocre>
2023-06-26 17:22:58 -0700

    strrev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: the string to reverse.
+ */
+void rev_string(char *s)
+{
+int len = 0;
+int i;
+char temp;
+
+len = _strlen(s);
+
+for (i = 0; i < len / 2; i++)
+{
+temp = *(s + i);
+*(s + i) = *(s + len - i - 1);
+*(s + len - i - 1) = temp;
+}
+}
