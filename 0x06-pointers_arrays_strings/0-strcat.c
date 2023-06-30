8a805ded119ea00060e6b8646a4e9ce4fe296e95
Madiocre <Madiocre>
2023-06-29 18:28:37 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,16 +5,14 @@
  * @src: The source string to be appended
  * Return: Pointer to the resulting string
  */
-char* _strcat(char *dest, const char *src) {
-char *ptr = dest;
-while (*ptr != '\0') {
-ptr++;
+char *_strcat(char *dest, char *src)
+{
+size_t dest_len = strlen(dest);
+size_t i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[dest_len + i] = src[i];
+dest[dest_len + i] = '\0';
 }
-while (*src != '\0') {
-*ptr = *src;
-ptr++;
-src++;
-}
-*ptr = '\0';
 return dest;
 }
