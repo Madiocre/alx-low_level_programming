cfd2dce2a99160de5ea0d1cc72682f646da86bfd
Madiocre <Madiocre>
2023-06-29 18:25:04 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,17 +3,18 @@
  * _strcat - Concatenates two strings
  * @dest: The destination string
  * @src: The source string to be appended
- *
  * Return: Pointer to the resulting string
  */
-char *_strcat(char *dest, char *src)
-{
-size_t dest_len = strlen(dest);
-size_t i;
-for (i = 0; src[i] != '\0'; i++)
-{
-dest[dest_len + i] = src[i];
-dest[dest_len + i] = '\0';
+char* _strcat(char *dest, const char *src) {
+char *ptr = dest;
+while (*ptr != '\0') {
+ptr++;
 }
+while (*src != '\0') {
+*ptr = *src;
+ptr++;
+src++;
+}
+*ptr = '\0';
 return dest;
 }
