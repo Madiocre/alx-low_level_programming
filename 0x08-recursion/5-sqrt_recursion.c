36b095ca9b30600a915e0fceff2f5db5500a5a32
Madiocre <Madiocre>
2023-07-06 09:40:15 -0700

    sqrt recursive with helper function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,7 +4,7 @@
  * @n: The number to calculate the square root of.
  * @start: The starting point for the search.
  * @end: The ending point for the search.
- * Return: The natural square root of the number, or -1                                                                                                                       
+ * Return: The natural square root of the number, or -1.
  */
 int sqrt_helper(int n, int start, int end)
 {
@@ -12,9 +12,9 @@ int mid, result;
 if (start <= end)
 {
 mid = (start + end) / 2;
-if (mid * mid == n)
+if (mid *mid == n)
 return (mid);
-if (mid * mid < n)
+if (mid *mid < n)
 {
 result = sqrt_helper(n, mid + 1, end);
 if (result != -1)
