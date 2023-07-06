0763c8021a2dcef55b30843660157ebd6542db9d
Madiocre <Madiocre>
2023-07-05 18:23:01 -0700

    put recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,10 +5,11 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\0') {
-putchar('\n');  // Print newline after the entire string is printed
+if (*s == '\0')
+{
+putchar('\n');
 return;
 }
-putchar(*s);  // Print the current character
-_puts_recursion(s + 1);  // Call the function recursively with the next character
+putchar(*s);
+_puts_recursion(s + 1);
 }
