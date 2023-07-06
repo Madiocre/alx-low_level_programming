f3f7294e9850308bc7c6b1744497d14435349b9f
Madiocre <Madiocre>
2023-07-05 18:17:53 -0700

    put recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string using recursion
+ * @s: The string to be printed
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\0') {
+_putchar('\n');  // Print newline after the entire string is printed
+return;
+}
+_putchar(*s);  // Print the current character
+_puts_recursion(s + 1);  // Call the function recursively with the next character
+}
