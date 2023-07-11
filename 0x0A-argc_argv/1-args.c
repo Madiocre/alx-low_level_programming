13a74e175d7db96064cd40a78974e0754bc57546
Madiocre <Madiocre>
2023-07-11 16:13:39 -0700

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments provided
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+  printf("%d\n", argc);
+  return (0);
+}
