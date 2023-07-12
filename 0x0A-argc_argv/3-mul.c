7d1155bc82c1b6a9561858ce46eba0f3461a45c1
Madiocre <Madiocre>
2023-07-12 16:51:29 -0700

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments 
+ *        (including the program name).
+ * @argv: An array of strings representing the command-line arguments.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[]) {
+int num1;
+int num2;
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+int result = num1 * num2;
+printf("%d\n", result);
+return (0);
+}
