00238ee200d0b2496bb20a18a4ce37024b04de75
Madiocre <Madiocre>
2023-07-12 16:57:49 -0700

    add

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,9 +12,9 @@
 int main(int argc, char *argv[]) {
 int num1;
 int num2;
+int result;
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-int result;
 if(argc == 3)
 result = num1 * num2;
 printf("%d\n", result);
