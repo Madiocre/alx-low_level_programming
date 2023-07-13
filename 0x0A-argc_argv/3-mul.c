133935fe8bd7928a8b7330815d3bb555e9455d92
Madiocre <Madiocre>
2023-07-12 17:01:01 -0700

    change

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,6 +13,10 @@ int main(int argc, char *argv[]) {
 int num1;
 int num2;
 int result;
+if (argc != 3) {
+printf("Error\n");
+return (1);
+}
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 if(argc == 3)
