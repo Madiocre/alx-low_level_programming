513b84f7ee8f9e2d7e2057d924c87f6cab2f4c22
Madiocre <Madiocre>
2023-07-14 18:39:42 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,31 +1,37 @@
 #include "main.h"
 /**
- * string_nconcat - Concatenates two strings up to a specified length.
+ * string_nconcat - Concatenates two strings.
  * @s1: The first string.
  * @s2: The second string.
- * @n: The maximum number of characters from s2 to concatenate.
+ * @n: The number of bytes from s2 to concatenate.
  *
  * Return: A pointer to the concatenated string, or NULL on failure.
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n) {
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = 0, len2 = 0;
+	char *concat;
 
-size_t s1_len = strlen(s1);
-size_t s2_len = strlen(s2);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-if (n >= s2_len)
-n = s2_len;
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-char *result = malloc((s1_len + n + 1) * sizeof(char));
-if (result == NULL)
-return (NULL);
+	if (n >= len2)
+		n = len2;
 
-strncpy(result, s1, s1_len);
-strncpy(result + s1_len, s2, n);
-result[s1_len + n] = '\0';
+	concat = malloc(sizeof(char) * (len1 + n + 1));
+	if (concat == NULL)
+		return (NULL);
 
-return (result);
+	memcpy(concat, s1, len1);
+	memcpy(concat + len1, s2, n);
+	concat[len1 + n] = '\0';
+
+	return (concat);
 }
