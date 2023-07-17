2d40a67b42ac968475c415d69e71e73e887cfa33
Madiocre <Madiocre>
2023-07-17 16:56:49 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure representing a dog.
+ * @name: Pointer to a character string representing the name of the dog.
+ * @age: Float value representing the age of the dog.
+ * @owner: Pointer to a character string representing the owner of the dog.
+ *
+ * Description: The `struct dog` structure represents a dog with its name, age,
+ *              and owner.
+ */
+typedef struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
+
+#endif /* DOG_H */
