931f9083e4ff0dfc1258b0ee19acd14dd4b304a6
Madiocre <Madiocre>
2023-07-18 15:04:30 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
-#include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 
 /**
@@ -9,25 +9,26 @@
  * @age: The age of the dog
  * @owner: The owner of the dog
  *
- * Return: Pointer to the new dog structure, or NULL if memory allocation fails.
+ * Return: Pointer to the new dog structure,
+ *         or NULL if memory allocation fails.
  *
- * Description: This function creates a new dog and initializes its fields with the provided details.
- *              It dynamically allocates memory for the name and owner strings and copies the provided
- *              name and owner values into the newly allocated memory.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *new_dog = malloc(sizeof(dog_t));
+int name_length;
+int owner_length;
+new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 return (NULL);
-int name_length = strlen(name) + 1;
+name_length = strlen(name) + 1;
 new_dog->name = malloc(name_length);
 if (new_dog->name == NULL) {
 free(new_dog);
 return (NULL);
 }
 strcpy(new_dog->name, name);
-int owner_length = strlen(owner) + 1;
+owner_length = strlen(owner) + 1;
 new_dog->owner = malloc(owner_length);
 if (new_dog->owner == NULL) {
 free(new_dog->name);
