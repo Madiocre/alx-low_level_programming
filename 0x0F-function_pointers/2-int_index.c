751f9f08ddb829ce00ba78abff84b229ff90bb23
Madiocre <Madiocre>
2023-07-21 13:58:00 -0700

    locating a number

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+ * int_index - Searches for an integer in an 
+ *             array using a custom comparison function.
+ * @array: Pointer to the array of integers.
+ * @size: Number of elements in the array.
+ * @cmp: Pointer to the comparison function.
+ *
+ * Return: Index of the first element for which the 
+ *         comparison function does not return 0;
+ *         -1 if no element matches or size <= 0.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+		return (-1);
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (int i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
