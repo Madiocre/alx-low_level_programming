119b82227e61f798f4ca56e1b64049614c3ca8f4
Madiocre <Madiocre>
2023-07-20 17:08:42 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,9 +4,7 @@
  * @name: The name to be printed
  * @f: Pointer to the function that will be used to print the name
  *
- * This function takes a name and a function pointer as parameters.
- * It calls the function pointed to by `f` and passes the name as an argument.
- * The function pointed to by `f` is responsible for printing the name.
+ * Return: no return
  */
 void print_name(char *name, void (*f)(char *))
 {
