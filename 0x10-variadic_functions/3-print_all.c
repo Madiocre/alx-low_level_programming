b68a8833fbe442278167b6d963ab8c632a0052e1
Madiocre <Madiocre>
2023-07-21 18:13:55 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,45 +12,43 @@
  */
 void print_all(const char * const format, ...)
 {
-
-	int i;
-	char c;
-	char *s;
-
+int i = 0;
+char c;
+char *s;
 va_list args;
+
 va_start(args, format);
-	while (format && format[i])
-	{
-		switch (format[i])
-		{
-			case 'c':
-				c = (char)va_arg(args, int);
-				printf("%c", c);
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", (double)va_arg(args, double));
-				break;
-			case 's':
-				s = va_arg(args, char *);
-				if (s == NULL)
-					printf("(nil)");
-				else
-					printf("%s", s);
-				break;
-			default:
-				i++;
-				continue;
-		}
+while (format[i] != '\0')
+{
+switch (format[i])
+{
+case 'c':
+c = (char)va_arg(args, int);
+printf("%c", c);
+break;
+case 'i':
+printf("%d", va_arg(args, int));
+break;
+case 'f':
+printf("%f", (double)va_arg(args, double));
+break;
+case 's':
+s = va_arg(args, char *);
+if (s == NULL)
+printf("(nil)");
+else
+printf("%s", s);
+break;
+default:
+break;
+}
 
-		if (format[i + 1] != '\0')
-			printf(", ");
+if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
+printf(", ");
 
-		i++;
-	}
+i++;
+}
 
-	va_end(args);
-	printf("\n");
+va_end(args);
+printf("\n");
 }
