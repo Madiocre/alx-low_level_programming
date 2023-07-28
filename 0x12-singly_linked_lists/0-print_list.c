7d6980d244cbbd8f8a7d3e4a15cd418976ec8bc1
Madiocre <Madiocre>
2023-07-28 14:20:09 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,17 @@ size_t print_list(const list_t *h)
 {
 const list_t *current = h;
 size_t count;
+count = 0;
 
 while(current != NULL)
 {
 if (current->str != NULL)
 {
-printf("[%ld] %s\n", count, current->str);
+  printf("[%u] %s\n", current->len, current->str);
 }
 else
 {
-printf("[%ld] (nil)\n", count);
+printf("[0] (nil)\n");
 }
 
 current = current->next;
