7723be12cc45fc8fde570e69db0c122d40938c0e
Madiocre <Madiocre>
2023-07-31 16:49:02 -0700

    list int

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 
 /**
- * list_len - Return no. of nodes in  linked list.
+ * listint_len - Returns the number of nodes in a listint_t linked list.
  * @h: A pointer to the head of the linked list.
- * Return: The number of nodes in the linked list
+ *
+ * Return: The number of nodes in the linked list.
  */
 size_t listint_len(const listint_t *h)
 {
 const listint_t *current = h;
-size_t count;
-count = 0;
+size_t count = 0;
 
 while (current != NULL)
 {
@@ -17,5 +17,5 @@ current = current->next;
 count++;
 }
 
-return (count);
+return count;
 }
