39bbb16a206d7a7e893197841b4e1ff0135e8e4c
Madiocre <Madiocre>
2023-08-01 01:59:29 -0700

    free list head null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * free_listint - Frees a listint_t list.
+ * free_listint2 - Frees a listint_t list.
  * @head: A pointer to the head of the list.
  *
  * Return: No Return
