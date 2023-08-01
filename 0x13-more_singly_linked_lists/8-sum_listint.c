2348069d33b29e54a7be13d646440ab10ccb400e
Madiocre <Madiocre>
2023-08-01 14:00:59 -0700

    sum all elements

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,11 @@
 #include "lists.h"
 
 /**
+ * sum_listint - Returns the sum of all the data(n).
+ * @head: A pointer to the head of the list.
  *
+ * Return: If the list is empty - 0.
+ *         Otherwise - the sum of all the data (n).
  */
 
 int sum_listint(listint_t *head)
