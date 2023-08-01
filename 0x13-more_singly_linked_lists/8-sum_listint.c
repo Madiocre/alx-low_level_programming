2946b8a5825816959bc4d85f59153a55aae72db5
Madiocre <Madiocre>
2023-08-01 13:59:14 -0700

    sum all elements

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *current;
+	int sum;
+
+	if (head == NULL)
+		return (0);
+
+	current = head;
+	sum = 0;
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
