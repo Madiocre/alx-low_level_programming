d77f2d979985261a57036232d48f7714b5e59413
Madiocre <Madiocre>
2023-08-02 07:52:34 -0700

    multiply 2 numbers after valdation

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int is_valid_number(char *str);
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ *
+ * Return: 0 on success, 98 on error
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (!is_valid_number(argv[1]) || !is_valid_number(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
+
+/**
+ * is_valid_number - Checks if a string is a valid number
+ * @str: The string to check
+ *
+ * Return: 1 if the string is a valid number, 0 otherwise
+ */
+int is_valid_number(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+
+	return (1);
+}
