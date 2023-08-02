3ebbf3450551d30f0d5221e84cf3b4097454ab62
Madiocre <Madiocre>
2023-08-01 17:59:47 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,12 +27,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	count = 0;
 	new_node->n = n;
 
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
 	current = *head;
 
 	while (current != NULL)
