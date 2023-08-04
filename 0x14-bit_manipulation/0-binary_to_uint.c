603c9779611a421585d5e16c1018ad88aeb88f23
Madiocre <Madiocre>
2023-08-03 17:32:12 -0700

    binary to unsigned

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -22,7 +22,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-        	if (b[i] != '0' && b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
@@ -30,5 +30,5 @@ unsigned int binary_to_uint(const char *b)
 	}
 
 	return (result);
-	
+
 }
