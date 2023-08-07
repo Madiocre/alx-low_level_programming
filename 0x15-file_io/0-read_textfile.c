ef7a64863d21d1b47f361d953e85537ad609f22f
Madiocre <Madiocre>
2023-08-07 16:27:24 -0700

    read and print

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,19 +15,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t write_content;
 	char *buffer;
 
-
 	if (filename == NULL)
 	{
 		return (0);
 	}
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		close(fd);
 		return (0);
 	}
-
 	buffer = malloc(sizeof(char) * (letters + 1));
 	if (buffer == NULL)
 	{
@@ -43,7 +40,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	buffer[read_content] = '\0';
-
 	write_content = write(STDOUT_FILENO, buffer, read_content);
 	if (write_content != read_content)
 	{
@@ -51,9 +47,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buffer);
 		return (0);
 	}
-
 	close(fd);
 	free(buffer);
-
 	return (write_content);
 }
