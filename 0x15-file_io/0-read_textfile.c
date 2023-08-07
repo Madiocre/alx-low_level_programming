52d88d7e2c8c3eb9431d0fbcf6b69b21f048fbd5
Madiocre <Madiocre>
2023-08-07 16:25:32 -0700

    read and print

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
 /**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: The name of the file to read
+ * @letters: The number of letters to read and print
  *
+ * Return: The actual number of letters read and printed, or 0 on failure
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
