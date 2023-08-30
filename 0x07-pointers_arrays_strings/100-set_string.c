ade66aceeba41393f4f2cc4c9455434ab5c5fc99
Madiocre <wolvirex@gmail.com>
2023-08-29 17:23:58 -0700

    added_set_strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets **s equal to *to
+ * @s: string to be changed
+ * @to: string to change to
+ *
+ * Return: No return
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
