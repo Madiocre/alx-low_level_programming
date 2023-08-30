078ba5ba3f47be6526c7067ddb965ee9ac7accd3
Madiocre <wolvirex@gmail.com>
2023-08-30 05:44:41 -0700

    recursion_added_task_6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,13 +8,13 @@
  * Return: 1 if the number is prime, 0 otherwise.
  */
 
-int is_prime_helper(int n, int divisor)
+int prime_helper(int n, int divisor)
 {
 	if (divisor >= n)
 		return (1);
 	if (n % divisor == 0)
 		return (0);
-	return (is_prime_helper(n, divisor + 1));
+	return (prime_helper(n, divisor + 1));
 }
 
 /**
@@ -28,5 +28,5 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (is_prime_helper(n, 2));
+	return (prime_helper(n, 2));
 }
