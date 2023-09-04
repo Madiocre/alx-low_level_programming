341f19a47de2ed6f468e0202ea0ee18750a986fc
Madiocre <wolvirex@gmail.com>
2023-09-04 12:26:40 -0700

    malloc_free_task_1_fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,17 +5,18 @@
  * _strdup - Duplicates a string.
  * @str: The string to duplicate.
  *
- * Return: On success, returns a pointer to the duplicated string.
- *         It returns NULL if str is NULL or if memory allocation fails.
+ * Return: On success, returns a pointer to the duplicated string,
+ *         or NULL if str is NULL or if memory allocation fails.
  */
 char *_strdup(char *str)
 {
-	size_t len = strlen(str);
+	size_t len;
 	char *dup_str;
 
 	if (str == NULL)
 		return (NULL);
 
+	len = strlen(str);
 	dup_str = malloc((len + 1) * sizeof(char));
 	if (dup_str == NULL)
 		return (NULL);
