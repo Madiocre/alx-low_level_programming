96013aad020adea40ab741839526d1e376da9779
Madiocre <wolvirex@gmail.com>
2023-09-07 16:59:49 -0700

    more_memory_task_2_3

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _calloc: Function that works like calloc
+ * @nmemb: Used to check validity
+ * @size: size of allocation
+ *
+ * Return: Return: On success, a pointer to the allocated memory.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
