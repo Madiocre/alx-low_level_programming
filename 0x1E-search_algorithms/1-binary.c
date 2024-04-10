13b786ec385e932c1a26280c63d84c2237c6cb17
madiocre <wolvirex@gmail.com>
2024-04-10 23:38:50 +0200

    search algo linear fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searching algoritm
+ * binary_search - Searching algoritm
  * @array: array to be searched
  * @size: size of said array
  * @value: value we are searching for
@@ -9,17 +9,11 @@
  * Return: The first index at which the value is found
  */
 
-int linear_search(int *array, size_t size, int value)
+int binary_search(int *array, size_t size, int value)
 {
 	size_t i;
 
 	if (array == NULL)
 		return (-1);
-	for (i = 0; i < size; i++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
-	return (-1);
+	
 }
